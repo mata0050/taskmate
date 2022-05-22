99cd246510974188239b16794fbcdb922a92ec1d
mata0050 <mata0050@outlook.com>
2022-05-21 22:51:34 -0400

    graphql setup

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "7285589000ed8647"; }
+/******/ 		__webpack_require__.h = function() { return "274eb680b46323b9"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
