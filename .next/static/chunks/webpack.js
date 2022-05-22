cf2780d7972d308bd653469c43ba94b96655281f
mata0050 <mata0050@outlook.com>
2022-05-21 22:06:18 -0400

    prismaa setup

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "4d59faeb16d8702c"; }
+/******/ 		__webpack_require__.h = function() { return "7285589000ed8647"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
--- tailwind.config.js
+++ tailwind.config.js
@@ -4,7 +4,38 @@ module.exports = {
     './components/**/*.{js,ts,jsx,tsx}',
   ],
   theme: {
-    extend: {},
+    screens: {
+      xsm: '480px',
+      sm: '640px',
+      md: '768px',
+      lg: '1024px',
+      xl: '1280px',
+      '2xl': '1536px',
+    },
+    colors: {
+      blue: '#1fb6ff',
+      pink: '#ff49db',
+      orange: '#ff7849',
+      green: '#13ce66',
+      'gray-dark': '#273444',
+      gray: {
+        100: '#f7fafc',
+        900: '#1a202c',
+      },
+    },
+    fontFamily: {
+      sans: ['Graphik', 'sans-serif'],
+      serif: ['Merriweather', 'serif'],
+    },
+    extend: {
+      spacing: {
+        128: '32rem',
+        144: '36rem',
+      },
+      borderRadius: {
+        '4xl': '2rem',
+      },
+    },
   },
   variants: {
     extend: {},
