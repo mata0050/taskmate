fa863a85352308930d9f1b2809c4f263e518a42e
mata0050 <mata0050@outlook.com>
2022-05-23 13:12:49 -0400

    auth0 setup

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -161,7 +161,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "74d3ebf8b51e9def"; }
+/******/ 		__webpack_require__.h = function() { return "2e9fd4931685737a"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,16 +1,19 @@
-import '../styles/tailwind.css'
-import Layout from '../components/Layout'
-import { ApolloProvider } from '@apollo/client'
-import apolloClient from '../lib/apollo'
+import '../styles/tailwind.css';
+import Layout from '../components/Layout';
+import { UserProvider } from '@auth0/nextjs-auth0';
+import { ApolloProvider } from '@apollo/client';
+import apolloClient from '../lib/apollo';
 
 function MyApp({ Component, pageProps }) {
   return (
-    <ApolloProvider client={apolloClient}>
-      <Layout>
-        <Component {...pageProps} />
-      </Layout>
-    </ApolloProvider>
-  )
+    <UserProvider>
+      <ApolloProvider client={apolloClient}>
+        <Layout>
+          <Component {...pageProps} />
+        </Layout>
+      </ApolloProvider>
+    </UserProvider>
+  );
 }
 
-export default MyApp
+export default MyApp;
